291cfefec2f619982606a5b888b225d0bf4ed28d
omarrraliii <omaralitaha98@gmail.com>
2025-05-11 01:25:42 +0300

    Implement post sharing feature and update views with class-based PostListView

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_details, name='post_details')
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),  # Using the class-based view for post list
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_details, name='post_details'),
+    path('<int:post_id>/share/', views.post_share, name='post_share')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,53 @@
 from django.http import Http404
 from django.shortcuts import render , get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 
-def post_list(request):                                                 #View for listing all the posts
-    post_list = Post.published.all()
-    paginator=Paginator(post_list, 3)                                          #Pagination of posts
-    page_number = request.GET.get('page',1)
-    posts=paginator.page(page_number)                                          #Getting the page number from the request
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+def post_share(request, post_id):
+    sent=False
+    post=get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)  
+    if request.method == 'POST':
+        form=EmailPostForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            post_url=request.build_absolute_uri(
+                post.get_absolute_url())
+            subject=f"{cd['name']} recommends you to read " \
+                    f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message,'dummy@gmail.com',
+                      [cd['to']])
+            sent=True  
+    else:
+        form=EmailPostForm()
+    return render(request, 'blog/post/share.html',{'post': post,
+                                                   'form': form,
+                                                   'sent': sent})
+class PostListView(ListView):
+    queryset = Post.published.all()                               #ListView for displaying the posts
+    context_object_name= 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+# def post_list(request):                                                 #View for listing all the posts
+#     post_list = Post.published.all()
+#     paginator=Paginator(post_list, 3)                                          
+#     page_number = request.GET.get('page',1)
+#     try:
+#         posts=paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts - paginator.page(paginator.num_pages)                                          #Getting the page number from the request
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'posts': posts})
 
 def post_details(request,year,month,day,post):                                           #View for displaying the details of a post
     post = get_object_or_404(Post,

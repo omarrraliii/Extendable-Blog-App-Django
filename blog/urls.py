b8a1d0dd4ba7a230050819d2d058247201e22a28
omarrraliii <omaralitaha98@gmail.com>
2025-05-18 01:21:10 +0300

    Add comment functionality with Comment model, forms, and views

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # path('', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),  # Using the class-based view for post list
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_details, name='post_details'),
-    path('<int:post_id>/share/', views.post_share, name='post_share')
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', views.post_comment, name= 'post_comment')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,27 @@
 from django.http import Http404
 from django.shortcuts import render , get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
+from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 
 # Create your views here.
+
+@require_POST
+def post_comment(request, post_id):
+    post= get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED) 
+    form=CommentForm(data=request.POST)
+    comment=None
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post=post
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post':post,
+                   'form':form,
+                   'comment': comment})
 
 def post_share(request, post_id):
     sent=False
@@ -56,6 +71,10 @@ def post_details(request,year,month,day,post):                                  
                              publish__day=day,
                              slug=post,
                              status=Post.Status.PUBLISHED)
+    comments = post.comments.filter(active=True)
+    form =CommentForm()                               
     return render(request,
                   'blog/post/details.html',
-                  {'post':post})
+                  {'post':post, 
+                   'form':form,
+                   'comments': comments})

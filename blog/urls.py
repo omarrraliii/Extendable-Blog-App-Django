8e1544d5b8253e823e0cfaf81214c2a23369333c
omarrraliii <omaralitaha98@gmail.com>
2025-05-28 18:15:31 +0300

    Add RSS feed support, sitemap integration, and PostgreSQL database configuration

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from .feeds import LatestPostsFeed
 app_name = 'blog'
 
 urlpatterns = [
@@ -9,5 +9,6 @@ urlpatterns = [
     path('tag/<slug:tag_slug>', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_details, name='post_details'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
-    path('<int:post_id>/comment/', views.post_comment, name= 'post_comment')
+    path('<int:post_id>/comment/', views.post_comment, name= 'post_comment'),
+    path('feed/',LatestPostsFeed(), name='post_feed'),
 ]
